a32da8ae8044fa75b8edc63411b80d394bb8662d
swordronin <78177433+swordronin@users.noreply.github.com>
2025-08-03 02:48:15 +0200

    create namin_conventions

---
+++ basics/variables.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "John"
+	// var name1 = "Jane"
+	//middleName := "Mayor"
+	fmt.Println(middleName)
+
+	// count := 10
+	// lastName := "Smith"
+
+	// Default Values
+	// Numeric Types : 0
+	// Boolean Types : false
+	// String Types : " "
+	// Pointer, slices, maps function and structs:nil
+
+	// ---- Scope
+
+	//fmt.Println(printName())
+	printName()
+
+}
+
+func printName() {
+	firstName := "Micheal"
+	fmt.Println(firstName)
+}
---
+++ namin_conventions.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	type Employee struct {
+		FirstName string
+		LastName  string
+		Age       int
+	}
+	// Pascal case
+	// Eg. CalculateArea, UserInfo, NewHTTPRequest
+	// Struct, interfacer, enums
+	//
+	// Snake_case
+	// EG. user_id, first_name, http_request
+	// UPPERCASE
+	// use case is Constants
+
+	// mixedCase
+	// Eg. javaScript, htmlDocuments, isValid
+	const MAXRETRIES = 5
+
+	var employeeID = 1001
+	fmt.Println("EmployeeID:", employeeID)
+}
